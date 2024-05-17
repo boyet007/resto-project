045fd440b4f7bd5fc05d726ee01f22c3bc94f5c2
Boyet Notebook <boyet007@gmail.com>
2024-05-17 13:59:31 +0700

    SIgnup Form

---
+++ src/components/SignUp.vue
@@ -0,0 +1,42 @@
+<template>
+    <img class="logo" src="../assets/resto-logo.jpg" />
+    <h1>Sign Up</h1>
+    <div class="register">
+        <input type="text" placeholder="Enter Name" name="" id="" />
+        <input type="text" placeholder="Enter Email" name="" id="" />
+        <input type="password" placeholder="Enter Password" name="" id="" />
+        <button>Sign Up</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SignUp",
+};
+</script>
+
+<style>
+.logo {
+    width: 100px;
+}
+
+.register input {
+    display: block;
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    margin-bottom: 12px;
+    border: 1px solid black;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+.register button {
+    cursor: pointer;
+    color: white;
+    background: skyblue;
+    border: 1px solid skyblue;
+    height: 40px;
+    width: 320px;
+}
+</style>
